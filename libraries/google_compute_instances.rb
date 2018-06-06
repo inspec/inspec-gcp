804e06778411f01b39c186326c047c7fa20f9639
Stuart Paterson <spaterson@chef.io>
2018-06-06 12:51:45 +0100

    Added missing documentation following InSpec standards, resolves #11. (#16)
    * Added missing documentation following InSpec standards, resolves #11.
    Also minor corrections to a couple of resources.
    Used InSpec FilterTable to create first plural resource for compute instances, including exammple controls and documentation.
    
    Signed-off-by: Stuart Paterson <spaterson@chef.io>
    
    * Rubocop indentation fix for google_compute_instances.
    
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

---
+++ libraries/google_compute_instances.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'gcp_backend'
+
+module Inspec::Resources
+  class GoogleComputeInstances < GcpResourceBase
+    name 'google_compute_instances'
+    desc 'Verifies settings for GCP compute instances in bulk'
+
+    example "
+      describe google_compute_instances(project: 'chef-inspec-gcp',  zone: 'europe-west2-a') do
+        it { should exist }
+        ...
+      end
+    "
+
+    def initialize(opts = {})
+      # Call the parent class constructor
+      super(opts)
+      @display_name = opts[:name]
+      @zone = opts[:zone]
+      @project = opts[:project]
+    end
+
+    # FilterTable setup
+    filter_table_config = FilterTable.create
+    filter_table_config.add_accessor(:where)
+    filter_table_config.add_accessor(:entries)
+    filter_table_config.add(:exist?) { |filter_table| !filter_table.entries.empty? }
+    filter_table_config.add(:count) { |filter_table| filter_table.entries.count }
+    filter_table_config.add(:instance_ids, field: :instance_id)
+    filter_table_config.add(:instance_names, field: :instance_name)
+    filter_table_config.add(:colors, field: :color, type: :simple)
+    filter_table_config.connect(self, :fetch_data)
+
+    def fetch_data
+      instance_rows = []
+      catch_gcp_errors do
+        @instances = @gcp.gcp_compute_client.list_instances(@project, @zone)
+      end
+      @instances.items.map do |instance|
+        instance_rows+=[{ instance_id: instance.id,
+                        instance_name: instance.name }]
+      end
+      @table = instance_rows
+    end
+  end
+end
---
+++ test/integration/verify/controls/google_compute_vms_loop.rb
@@ -0,0 +1,20 @@
+title 'Loop over all GCP Virtual Machines'
+
+gcp_project_id = attribute(:gcp_project_id, default: '', description: 'The GCP project identifier.')
+gcp_zone = attribute(:gcp_zone, default: '', description: 'The GCP zone being used.')
+
+control 'gcp-vms-loop-1.0' do
+
+  impact 1.0
+  title 'Ensure VMs have the correct properties in bulk using google_compute_instance for detail.'
+
+  google_compute_instances(project: gcp_project_id, zone: gcp_zone).instance_names.each do |instance_name|
+    describe google_compute_instance(project: gcp_project_id, zone: gcp_zone, name: instance_name) do
+      it { should exist }
+      its('zone') { should match gcp_zone }
+      its('kind') { should eq "compute#instance" }
+      its('status') { should eq 'RUNNING' }
+      its('tag_count'){ should be >= 1 }
+    end
+  end
+end
