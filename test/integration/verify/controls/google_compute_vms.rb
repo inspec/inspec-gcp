30e03bf9f5e739ed3a2fb3acb12ce4e71168a34e
Stuart Paterson <spaterson@chef.io>
2018-06-12 19:29:45 +0100

    Ensure “ids” parameters for plural resource are tested. Add configuration flag for optionally executing tests that rely on gcloud/grep. Correct zones filter criteria and properties documentation.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- test/integration/verify/controls/google_compute_vms.rb
+++ test/integration/verify/controls/google_compute_vms.rb
@@ -3,9 +3,11 @@ title 'Virtual Machines Properties'
 gcp_project_id = attribute(:gcp_project_id, default: '', description: 'The GCP project identifier.')
 gcp_zone = attribute(:gcp_zone, default: '', description: 'The GCP zone being used.')
 gcp_ext_vm_data_disk_name = attribute(:gcp_ext_vm_data_disk_name, default: '', description: 'A valid GCP VM name to check for.')
+gcp_enable_gcloud_calls = attribute(:gcp_enable_gcloud_calls,default:0,description:'Flag to enable the use of gcloud command line to pull in live data to test against.')
 
 control 'gcp-vms-1.0' do
 
+  only_if { gcp_enable_gcloud_calls.to_i == 1 }
   impact 1.0
   title 'Ensure VMs have the correct properties in bulk'
 
@@ -14,6 +16,9 @@ control 'gcp-vms-1.0' do
     its('count') { should be <= 100}
     its('instance_ids.count') { should cmp 9 }
     its('instance_names') { should include gcp_ext_vm_data_disk_name }
+    # Only make the call if the configuration flag is specified and the test will run
+    gcp_instance_id = `gcloud compute instances list --filter="name=gcp-inspec-ext-linux-vm" --format=json | grep id | grep -o '[0-9]\\+'`.chomp.to_i
+    its('instance_ids') { should include gcp_instance_id }
   end
 
 end
