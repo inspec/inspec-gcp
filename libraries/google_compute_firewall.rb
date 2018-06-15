4a7eea7f71f50c1aa62f6db41cbc5c3e121ae59a
Stuart Paterson <spaterson@chef.io>
2018-06-15 14:16:19 +0100

    Improved firewall rule handling, provides initial implementation for #27.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- libraries/google_compute_firewall.rb
+++ libraries/google_compute_firewall.rb
@@ -27,28 +27,139 @@ module Inspec::Resources
 
     # Check whether the firewall rule allows HTTP access (tcp ingress on port 80)
     def allowed_http?
-      ports_protocol_allowed(['80'])
+      port_protocol_allowed('80')
     end
 
     # Check whether the firewall rule allows SSH access (tcp ingress on port 22)
     def allowed_ssh?
-      ports_protocol_allowed(['22'])
+      port_protocol_allowed('22')
     end
 
     def allowed_https?
-      ports_protocol_allowed(['443'])
+      port_protocol_allowed('443')
     end
 
-    def ports_protocol_allowed(port_list, protocol = 'tcp', index = 0)
-      if defined?(allowed[index].item[:ip_protocol])
-        # so here we know at least there's some protocol/port to check
-        return false if allowed[index].item[:ip_protocol]!=protocol
-        # we know there's only one port in the list but let's assume in the future there could be several ports and order doesn't matter
-        return false if allowed[index].item[:ports].sort!=port_list.sort
-        true
-      else
+    def allow_port_protocol?(port, protocol)
+      port_protocol_allowed(port, protocol)
+    end
+    RSpec::Matchers.alias_matcher :allow_port_protocol, :be_allow_port_protocol
+
+    # initial implementation allows to search for target source and destination tags - can
+    # filter plural firewalls based on direction to pin down the desired rules and choose the appropriate method
+    # see similar below example for ip_range_list
+    def allow_source_tags?(tag_list)
+      return false if !defined?(source_tags)
+      match_list_helper(source_tags, tag_list)
+    end
+    RSpec::Matchers.alias_matcher :allow_source_tags, :be_allow_source_tags
+
+    def allow_target_tags?(tag_list)
+      return false if !defined?(target_tags)
+      match_list_helper(target_tags, tag_list)
+    end
+    RSpec::Matchers.alias_matcher :allow_target_tags, :be_allow_target_tags
+
+    def allow_source_tags_only?(tag_list)
+      return false if !defined?(source_tags)
+      match_list_helper(source_tags, tag_list, true)
+    end
+    RSpec::Matchers.alias_matcher :allow_source_tags_only, :be_allow_source_tags_only
+
+    def allow_target_tags_only?(tag_list)
+      return false if !defined?(target_tags)
+      match_list_helper(target_tags, tag_list, true)
+    end
+    RSpec::Matchers.alias_matcher :allow_target_tags_only, :be_allow_target_tags_only
+
+    def match_list_helper(source_list, target_list, only = false)
+      # helps streamline matching exact equality versus inclusion of target and source lists
+      if only # i.e. exact equality
+        return true if source_list.sort == target_list.sort
         false
+      else # check the source list includes at least all specified target list elements
+        target_list.each do |must_be_present|
+          return false if !source_list.include? must_be_present
+        end
+        true
       end
+    end
+
+    # initial implementation is direction agnostic and treats IP ranges separately
+    # Can revisit in the future to include AWS style matchers such as allow_in/out with multiple criteria
+    # including protocols/ports for example
+    def allow_ip_ranges_only?(ip_range_list)
+      allow_ip_range_list(ip_range_list, true)
+    end
+    RSpec::Matchers.alias_matcher :allow_ip_ranges_only, :be_allow_ip_ranges_only
+
+    def allow_ip_ranges?(ip_range_list)
+      allow_ip_range_list(ip_range_list)
+    end
+    RSpec::Matchers.alias_matcher :allow_ip_ranges, :be_allow_ip_ranges
+
+    def allow_ip_range_list(ip_range_list, only = false)
+      raise Inspec::Exceptions::ResourceFailed, "google_compute_firewall is missing expected property 'direction'" if !defined?(direction)
+      # the intention here is for firewall rules plural to be filtered based on direction, then tested for particular IP ranges
+      # e.g.        describe google_compute_firewalls(project: 'chef-inspec-gcp').where(firewall_direction: 'INGRESS').firewall_names.each do |firewall_name| do
+      #               describe google_compute_firewall(project: 'chef-inspec-gcp',  name: firewall_name) do
+      #                 it { should_not allow_ip_ranges ["0.0.0.0/0"] }
+      #               end
+      #             end
+      # direction affects what the property is e.g. INGRESS->source_ranges, EGRESS->destination_ranges
+      ranges = nil
+      if direction == 'INGRESS'
+        return false if !defined?(source_ranges)
+        ranges = source_ranges
+      else
+        return false if !defined?(destination_ranges)
+        ranges = destination_ranges
+      end
+      return false if ranges.nil?
+      # so now we have a list of IP addresses to compare
+      match_list_helper(ranges, ip_range_list, only)
+    end
+
+    # note that port_list only accepts individual ports to match, not ranges
+    def port_protocol_allowed(single_port, protocol = 'tcp')
+      raise Inspec::Exceptions::ResourceFailed, "google_compute_firewall is missing expected property 'allowed'" if !defined?(allowed)
+      # "allowed" can have several port/protocol pairing entries e.g. tcp:80 or udp:4000-5000
+      # first, let's find the matching protocol indexes to compare against
+      protocol_match_indexes = []
+      allowed.each_with_index do |rule, index|
+        next if !defined?(rule.item[:ip_protocol])
+        protocol_match_indexes<<index if rule.item[:ip_protocol]==protocol
+      end
+      # Now we know the list of matching protocol entries to check against.
+      # Note the syntax for protocol port definitions versus what we see here is:
+      # "tcp:80" -> "tcp" ["80"]
+      # "tcp:90,91" -> "tcp:90", "tcp:91" -> "tcp" ["90","91"]
+      # "udp:3000-4000" -> "udp" ["3000-4000"] # can also potentially include single port combinations in the list for a single rule here
+      # We now check for a match based on each of the above cases
+      protocol_match_indexes.each do |protocol_index|
+        # there can be multiple protocol rules for different ports etc. ["22"] or ["123-126"]
+        ports_in_rule = allowed[protocol_index].item[:ports]
+        ports_in_rule.each do |rule_port|
+          return true if single_port_matches(rule_port, single_port)
+        end
+      end
+      false
+    end
+
+    def single_port_matches(rule_port, single_port)
+      # if '-' in there it means we should check each provided port for existence in a range
+      if !rule_port.include? '-'
+        # simplest case, only one port string specified
+        return true if rule_port==single_port
+        # if not, no match
+      else
+        # the rule_port here is a range such as "4000-5000", protect against any non-integer input by checking for nil values
+        upper_limit = rule_port.split('-')[1]
+        lower_limit = rule_port.split('-')[0]
+        raise Inspec::Exceptions::ResourceFailed, "google_compute_firewall unexpected port range specified: '#{rule_port}'" if upper_limit.nil? || lower_limit.nil?
+        return true if single_port.to_i.between?(lower_limit, upper_limit)
+        # if not, no match
+      end
+      false
     end
 
     def exists?
--- test/integration/verify/controls/google_lb_internal_firewall_rule_mig1_ssh.rb
+++ test/integration/verify/controls/google_lb_internal_firewall_rule_mig1_ssh.rb
@@ -1,27 +1,27 @@
 title 'Firewall Rule Properties'
 
-gcp_project_id = attribute(:gcp_project_id, default: '', description: 'The GCP project identifier.')
-gcp_lb_mig1_name = attribute(:gcp_lb_mig1_name, default: '', description: 'The GCP pattern first managed instance group firewall name.')
+  gcp_project_id = attribute(:gcp_project_id, default: '', description: 'The GCP project identifier.')
+  gcp_lb_mig1_name = attribute(:gcp_lb_mig1_name, default: '', description: 'The GCP pattern first managed instance group firewall name.')
 
-control 'google-lb-internal-firewall-rule-ssh-1.0' do
+  control 'google-lb-internal-firewall-rule-ssh-1.0' do
 
-  impact 1.0
-  title 'Ensure that the Firewall Rule has been configured correctly'
+    impact 1.0
+    title 'Ensure that the Firewall Rule has been configured correctly'
 
-  describe google_compute_firewall(project: gcp_project_id, name: "#{gcp_lb_mig1_name}-vm-ssh") do
-    # TBD: could look at all rules / rules for e.g. a compute instance / a list of rule names instead of one-by-one
-    its('name') { should eq "#{gcp_lb_mig1_name}-vm-ssh" }
-    # For the currently in scope rule, SSH should be allowed
-    its('allowed_ssh?')  { should be true }
-    # For the currently in scope rule, HTTP should not be allowed
-    its('allowed_http?')  { should be false }
-    # the rule direction - ingress/egress
-    its('direction') { should eq "INGRESS" }
-    its('kind') { should eq "compute#firewall" }
+    describe google_compute_firewall(project: gcp_project_id, name: "#{gcp_lb_mig1_name}-vm-ssh") do
+      # TBD: could look at all rules / rules for e.g. a compute instance / a list of rule names instead of one-by-one
+      its('name') { should eq "#{gcp_lb_mig1_name}-vm-ssh" }
+      # For the currently in scope rule, SSH should be allowed
+      its('allowed_ssh?')  { should be true }
+      # For the currently in scope rule, HTTP should not be allowed
+      its('allowed_http?')  { should be false }
+      # the rule direction - ingress/egress
+      its('direction') { should eq "INGRESS" }
+      its('kind') { should eq "compute#firewall" }
 
-    # source IP range list for the rule
-    its('source_ranges') { should eq ["0.0.0.0/0"] }
+      # source IP range list for the rule
+      its('source_ranges') { should eq ["0.0.0.0/0"] }
 
-    # TBD: left out priority and network for now
+      # TBD: left out priority and network for now
+    end
   end
-end
