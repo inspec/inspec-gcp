7b84f962e582b8ccabb1062a2f8a0c504b4e66fd
Stuart Paterson <spaterson@chef.io>
2018-08-23 14:49:07 +0100

    Readability fix in google_compute_networks test.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- test/integration/verify/controls/google_compute_networks.rb
+++ test/integration/verify/controls/google_compute_networks.rb
@@ -1,4 +1,4 @@
-title 'Zones Properties'
+title 'Networks Properties'
 
 gcp_project_id = attribute(:gcp_project_id, default: '', description: 'The GCP project identifier.')
 gcp_network_name = attribute(:gcp_network_name, default: '', description: 'The GCP network name.')
@@ -6,7 +6,7 @@ gcp_network_name = attribute(:gcp_network_name, default: '', description: 'The G
 control 'gcp-networks-1.0' do
 
   impact 1.0
-  title 'Ensure zones have the correct properties in bulk'
+  title 'Ensure networks have the correct properties in bulk'
 
   describe google_compute_networks(project: gcp_project_id) do
     it { should exist }
