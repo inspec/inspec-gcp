5b89edcbb3a454d0beef71c9e76f76190cad01ef
jnahelou <nahelou.j@sfeir.com>
2018-11-29 14:22:51 +0100

    Fixes Use config to define test location
    Signed-off-by: jnahelou <nahelou.j@sfeir.com>

--- test/integration/verify/controls/google_compute_region_instance_group_managers.rb
+++ test/integration/verify/controls/google_compute_region_instance_group_managers.rb
@@ -1,14 +1,15 @@
 title 'Google compute region instance group managers properties'
 
 gcp_project_id = attribute(:gcp_project_id, default: '', description: 'The GCP project identifier.')
+gcp_lb_region = attribute(:gcp_lb_region, default: '', description: 'The GCP region being used.')
 
 control 'gcp-compute-region-instance-group-managers-1.0' do
 
   impact 1.0
   title 'Ensure compute regional instance group managers have the correct properties in bulk'
 
-  google_compute_region_instance_group_managers(project: gcp_project_id, region: 'europe-west2').instance_group_manager_names.each do |instance_group_manager_name|
-    describe google_compute_region_instance_group_manager(project: gcp_project_id, region: 'europe-west2', name: instance_group_manager_name) do 
+  google_compute_region_instance_group_managers(project: gcp_project_id, region: gcp_lb_region).instance_group_manager_names.each do |instance_group_manager_name|
+    describe google_compute_region_instance_group_manager(project: gcp_project_id, region: gcp_lb_region, name: instance_group_manager_name) do
       it { should exist }
       its('name') { should cmp /rigm/ }
       its('instance_template') { should cmp /itpl/ }
