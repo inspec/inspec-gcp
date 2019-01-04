71614c092043f017fcc429d5910ed665b5527260
Sam Levenick <slevenick@google.com>
2019-01-04 23:56:25 +0000

    Add InSpec disk resource, refactors of attributes within examples and documentation
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- test/integration/verify/controls/google_compute_ssl_policies.rb
+++ test/integration/verify/controls/google_compute_ssl_policies.rb
@@ -14,22 +14,28 @@
 
 title 'Test GCP google_compute_ssl_policies resource.'
 
-project_name = attribute(:gcp_project_name, default: '')
-ssl_policy = attribute('ssl_policy', default: {"name"=>"inspec-gcp-ssl-policy", "min_tls_version"=>"TLS_1_2", "profile"=>"CUSTOM", "custom_feature"=>"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "custom_feature2"=>"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"})
+gcp_project_id = attribute(:gcp_project_id, default: 'gcp_project_id', description: 'The GCP project identifier.')
+ssl_policy = attribute('ssl_policy', default: {
+  "name": "inspec-gcp-ssl-policy",
+  "min_tls_version": "TLS_1_2",
+  "profile": "CUSTOM",
+  "custom_feature": "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+  "custom_feature2": "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+})
 control 'google_compute_ssl_policies-1.0' do
   impact 1.0
   title 'google_compute_ssl_policies resource test'
 
-  describe google_compute_ssl_policies({project: project_name}) do
+  describe google_compute_ssl_policies(project: gcp_project_id) do
     it { should exist }
     its('names') { should include ssl_policy['name'] }
     its('profiles') { should include ssl_policy['profile'] }
     its('count') { should eq 1 }
   end
 
-  google_compute_ssl_policies({project: project_name}).names.each do |policy_name|
-    describe google_compute_ssl_policy({project: project_name, name: policy_name}) do
-      its('min_tls_version') { should cmp ssl_policy['min_tls_version'] }
+  google_compute_ssl_policies(project: gcp_project_id).names.each do |policy_name|
+    describe google_compute_ssl_policy({project: gcp_project_id, name: policy_name}) do
+      its('min_tls_version') { should eq ssl_policy['min_tls_version'] }
     end
   end
 end
