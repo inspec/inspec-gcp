b92be4f086a3ec4d45d0f2641a6fb2ceaa4ee6df
Sam Levenick <slevenick@google.com>
2019-01-04 22:44:20 +0000

    Commit test
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- test/integration/verify/controls/google_compute_ssl_policy.rb
+++ test/integration/verify/controls/google_compute_ssl_policy.rb
@@ -14,16 +14,16 @@
 
 title 'Test GCP google_compute_ssl_policy resource.'
 
-gcp_project_id = attribute(:gcp_project_id, default: '', description: 'The GCP project identifier.')
-ssl_policy = attribute('ssl_policy', default: { "name" => "inspec-gcp-ssl-policy", "min_tls_version" => "TLS_1_2", "profile" => "CUSTOM", "custom_feature" => "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "custom_feature2" => "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384" })
+project_name = attribute(:gcp_project_name, default: '')
+ssl_policy = attribute('ssl_policy', default: {"name"=>"inspec-gcp-ssl-policy", "min_tls_version"=>"TLS_1_2", "profile"=>"CUSTOM", "custom_feature"=>"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "custom_feature2"=>"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"})
 control 'google_compute_ssl_policy-1.0' do
   impact 1.0
   title 'google_compute_ssl_policy resource test'
 
-  describe google_compute_ssl_policy(project: gcp_project_id, name: ssl_policy['name']) do
+  describe google_compute_ssl_policy({project: project_name, name: ssl_policy['name']}) do
     it { should exist }
-    its('min_tls_version') { should eq ssl_policy['min_tls_version'] }
-    its('profile') { should eq ssl_policy['profile'] }
+    its('min_tls_version') { should cmp ssl_policy['min_tls_version'] }
+    its('profile') { should cmp ssl_policy['profile'] }
     its('custom_features') { should include ssl_policy['custom_feature'] }
     its('custom_features') { should include ssl_policy['custom_feature2'] }
   end
