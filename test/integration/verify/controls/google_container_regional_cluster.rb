d210fd62853a4bb5cbf28548666bbe17ac9e7816
Stuart Paterson <spaterson@chef.io>
2019-03-05 09:56:46 +0000

    Update google_container_regional_cluster control to use correct region.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- test/integration/verify/controls/google_container_regional_cluster.rb
+++ test/integration/verify/controls/google_container_regional_cluster.rb
@@ -15,22 +15,23 @@
 title 'Test GCP google_container_regional_cluster resource.'
 
 gcp_project_id = attribute(:gcp_project_id, default: 'gcp_project_id', description: 'The GCP project identifier.')
+gcp_region = attribute(:gcp_location, default: '', description: 'The GCP region being used.')
+
 regional_cluster = attribute('regional_cluster', default: {
   "name": "inspec-gcp-regional-cluster",
-  "region": "europe-west2",
   "initial_node_count": 1
 }, description: 'Regional container cluster definition')
 control 'google_container_regional_cluster-1.0' do
   impact 1.0
   title 'google_container_regional_cluster resource test'
 
-  describe google_container_regional_cluster(project: gcp_project_id, location: regional_cluster['region'], name: regional_cluster['name']) do
+  describe google_container_regional_cluster(project: gcp_project_id, location: gcp_region, name: regional_cluster['name']) do
     it { should exist }
     its('initial_node_count') { should eq regional_cluster['initial_node_count']}
-    its('location') { should eq regional_cluster['region']}
+    its('location') { should eq gcp_region }
   end
 
-  describe google_container_regional_cluster(project: gcp_project_id, location: regional_cluster['region'], name: 'nonexistent') do
+  describe google_container_regional_cluster(project: gcp_project_id, location: gcp_region, name: 'nonexistent') do
     it { should_not exist }
   end
 end
