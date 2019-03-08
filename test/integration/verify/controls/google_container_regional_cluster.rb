69e1071cf03158e2adceae04c383c72b552a5fd6
Sam Levenick <slevenick@google.com>
2019-03-08 18:41:13 +0000

    Use existing gcp_location attribute
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- test/integration/verify/controls/google_container_regional_cluster.rb
+++ test/integration/verify/controls/google_container_regional_cluster.rb
@@ -15,22 +15,22 @@
 title 'Test GCP google_container_regional_cluster resource.'
 
 gcp_project_id = attribute(:gcp_project_id, default: 'gcp_project_id', description: 'The GCP project identifier.')
+gcp_location = attribute(:gcp_location, default: 'gcp_location', description: 'The GCP project region.')
 regional_cluster = attribute('regional_cluster', default: {
   "name": "inspec-gcp-regional-cluster",
-  "region": "europe-west2",
   "initial_node_count": 1
 }, description: 'Regional container cluster definition')
 control 'google_container_regional_cluster-1.0' do
   impact 1.0
   title 'google_container_regional_cluster resource test'
 
-  describe google_container_regional_cluster(project: gcp_project_id, location: regional_cluster['region'], name: regional_cluster['name']) do
+  describe google_container_regional_cluster(project: gcp_project_id, location: gcp_location, name: regional_cluster['name']) do
     it { should exist }
     its('initial_node_count') { should eq regional_cluster['initial_node_count']}
-    its('location') { should eq regional_cluster['region']}
+    its('location') { should eq gcp_location}
   end
 
-  describe google_container_regional_cluster(project: gcp_project_id, location: regional_cluster['region'], name: 'nonexistent') do
+  describe google_container_regional_cluster(project: gcp_project_id, location: gcp_location, name: 'nonexistent') do
     it { should_not exist }
   end
 end
