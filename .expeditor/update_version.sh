f4dd153e87b68b812348c3642c4121a0f4abaf5e
Miah Johnson <miah@chia-pet.org>
2019-07-17 16:38:30 -0700

    Add expeditor and buildkite verify pipeline
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

---
+++ .expeditor/update_version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# After a PR merge, Chef Expeditor will bump the PATCH version in the VERSION file.
+# It then executes this file to update any other files/components with that new version.
+#
+
+set -ev
+
+sed -i -r "s/version\: .*/version\: $(cat VERSION)/" inspec.yml
