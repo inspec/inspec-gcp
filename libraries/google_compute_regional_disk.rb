f59f248e5e1ea86532a939eb4217831dcef322f4
Walter Dolce <walterdolce@gmail.com>
2019-08-23 12:02:10 +0100

    Fix missing instructions about comment in regional disk resource file
    Signed-off-by: Walter Dolce <walterdolce@gmail.com>

--- libraries/google_compute_regional_disk.rb
+++ libraries/google_compute_regional_disk.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: false
+
 require 'gcp_backend'
 require 'google/compute/property/disk_disk_encryption_key'
 require 'google/compute/property/disk_source_image_encryption_key'
