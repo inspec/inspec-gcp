19dc528ebc9028660efe20e404f501a7138649b8
Sam Levenick <slevenick@google.com>
2019-08-28 20:50:28 +0000

    Add new fields to container cluster api.yaml
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- libraries/google/container/property/regionalcluster_addons_config.rb
+++ libraries/google/container/property/regionalcluster_addons_config.rb
@@ -15,6 +15,7 @@
 # ----------------------------------------------------------------------------
 require 'google/container/property/regionalcluster_addons_config_horizontal_pod_autoscaling'
 require 'google/container/property/regionalcluster_addons_config_http_load_balancing'
+require 'google/container/property/regionalcluster_addons_config_kubernetes_dashboard'
 require 'google/container/property/regionalcluster_addons_config_network_policy_config'
 module GoogleInSpec
   module Container
@@ -24,6 +25,8 @@ module GoogleInSpec
 
         attr_reader :horizontal_pod_autoscaling
 
+        attr_reader :kubernetes_dashboard
+
         attr_reader :network_policy_config
 
         def initialize(args = nil, parent_identifier = nil)
@@ -31,6 +34,7 @@ module GoogleInSpec
           @parent_identifier = parent_identifier
           @http_load_balancing = GoogleInSpec::Container::Property::RegionalClusterAddonsConfigHttpLoadBalancing.new(args['httpLoadBalancing'], to_s)
           @horizontal_pod_autoscaling = GoogleInSpec::Container::Property::RegionalClusterAddonsConfigHorizontalPodAutoscaling.new(args['horizontalPodAutoscaling'], to_s)
+          @kubernetes_dashboard = GoogleInSpec::Container::Property::RegionalClusterAddonsConfigKubernetesDashboard.new(args['kubernetesDashboard'], to_s)
           @network_policy_config = GoogleInSpec::Container::Property::RegionalClusterAddonsConfigNetworkPolicyConfig.new(args['networkPolicyConfig'], to_s)
         end
 
