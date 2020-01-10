5d466ab738d2565eea42b2c7623f9eb303a5b649
Sam Levenick <slevenick@google.com>
2020-01-09 16:14:10 -0800

    Move KMS tests to handwritten, use google_projects.project_ids instead of project_names in loop test
    Signed-off-by: Sam Levenick <slevenick@google.com>

--- test/integration/verify/controls/google_projects_firewalls_loop.rb
+++ test/integration/verify/controls/google_projects_firewalls_loop.rb
@@ -5,7 +5,7 @@ control 'gcp-projects-firewalls-loop-1.0' do
   impact 1.0
   title 'Ensure INGRESS firewalls in all projects have the correct properties using google_compute_firewall for detail.'
 
-  google_projects.project_names.each do |project_name|
+  google_projects.project_ids.each do |project_name|
     google_compute_firewalls(project: project_name).where(firewall_direction: 'INGRESS').firewall_names.each do |firewall_name|
       describe google_compute_firewall(project: project_name, name: firewall_name) do
         it { should exist }
