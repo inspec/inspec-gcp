5b08b78841b728dc60e1dadd4f001f260cd2b0b3
The Magician <magic-modules@google.com>
2020-11-16 09:36:44 -0800

    Updated tests to use disk from config connector docs (#4235) (#474)
    https://cloud.google.com/config-connector/docs/reference/resource-docs/compute/computeimage
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- test/integration/verify/controls/google_compute_image.rb
+++ test/integration/verify/controls/google_compute_image.rb
@@ -17,7 +17,7 @@ title 'Test GCP google_compute_image resource.'
 gcp_project_id = attribute(:gcp_project_id, default: 'gcp_project_id', description: 'The GCP project identifier.')
 compute_image = attribute('compute_image', default: {
   "name": "inspec-image",
-  "source": "https://storage.googleapis.com/bosh-cpi-artifacts/bosh-stemcell-3262.4-google-kvm-ubuntu-trusty-go_agent-raw.tar.gz"
+  "source": "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
 }, description: 'Compute image description')
 control 'google_compute_image-1.0' do
   impact 1.0
