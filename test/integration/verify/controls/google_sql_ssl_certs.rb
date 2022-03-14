64e6d63501b0837d59bbaa7c0dd834c25ed5cd89
Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>
2022-03-14 10:27:09 +0530

    Update test/integration/verify/controls/google_sql_ssl_certs.rb

--- test/integration/verify/controls/google_sql_ssl_certs.rb
+++ test/integration/verify/controls/google_sql_ssl_certs.rb
@@ -15,7 +15,7 @@
 title 'Test GCP google_sql_ssl_certs resource.'
 
 gcp_project_id = attribute(:gcp_project_id, default: 'gcp_project_id', description: 'The GCP project identifier.')
-sql_connect = attribute('sql_connect', value: {
+sql_connect = attribute('sql_connect', default: {
   "region": "us-central1",
   "database_version": "POSTGRES_13",
   "backend_type": "SECOND_GEN",
