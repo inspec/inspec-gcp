3fc776a7ade87f0dd64b5209a72305d7fa81c1d2
Samir Anand <sam.sunny001@gmail.com>
2023-08-29 08:19:39 +0530

    Automatically generated by magic modules for service: compute and resource: RegionSecurityPolicy. This commit includes the following changes: - Singular Resource - Plural Resource - Documentation updates - Terraform configuration - Integration tests
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- build/inspec/test/integration/build/gcp-mm.tf
+++ build/inspec/test/integration/build/gcp-mm.tf
@@ -1402,3 +1402,165 @@ resource "google_compute_image" "example" {
   }
 }
 
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
