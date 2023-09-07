7e0cc70227653f39e7c1bb9ef455124234b07aa5
Samir Anand <sam.sunny001@gmail.com>
2023-09-07 18:54:19 +0530

    Automatically generated by magic modules for service: compute and resource: RegionSecurityPolicy. This commit includes the following changes: - Singular Resource - Plural Resource - Documentation updates - Terraform configuration - Integration tests
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- build/inspec/test/integration/build/gcp-mm.tf
+++ build/inspec/test/integration/build/gcp-mm.tf
@@ -1346,41 +1346,56 @@ resource "google_data_loss_prevention_stored_info_type" "basic" {
   }
 }
 
+resource "google_compute_region_security_policy" "default" {
+  provider    = google-beta
+  
+  region      = "us-west2"
+  name        = "policyruletest-${local.name_suffix}"
+  description = "basic region security policy"
+  type        = "CLOUD_ARMOR"
+}
+
+resource "google_compute_region_security_policy_rule" "policy_rule" {
+  provider = google-beta
+  
+  region          = "us-west2"
+  security_policy = google_compute_region_security_policy.default.name
+  description     = "new rule"
+  priority        = 100
+  match {
+    versioned_expr = "SRC_IPS_V1"
+    config {
+      src_ip_ranges = ["10.10.0.0/16"]
+    }
+  }
+  action          = "allow"
+  preview         = true
+}
 
 
+resource "google_compute_region_security_policy" "default" {
+  provider    = google-beta
+  
+  region      = "us-west2"
+  name        = "policyruletest-${local.name_suffix}"
+  description = "basic region security policy"
+  type        = "CLOUD_ARMOR"
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+resource "google_compute_region_security_policy_rule" "policy_rule" {
+  provider = google-beta
+  
+  region          = "us-west2"
+  security_policy = google_compute_region_security_policy.default.name
+  description     = "new rule"
+  priority        = 100
+  match {
+    versioned_expr = "SRC_IPS_V1"
+    config {
+      src_ip_ranges = ["10.10.0.0/16"]
+    }
+  }
+  action          = "allow"
+  preview         = true
+}
 
