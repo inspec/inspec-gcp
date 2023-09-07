b0f97a2145d70a8a959c6a3fa7575a2ffe75d52c
Samir Anand <sam.sunny001@gmail.com>
2023-09-07 19:04:39 +0530

    Automatically generated by magic modules for service: compute and resource: PublicDelegatedPrefix. This commit includes the following changes: - Singular Resource - Plural Resource - Documentation updates - Terraform configuration - Integration tests
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- docs/resources/google_compute_public_delegated_prefixes.md
+++ docs/resources/google_compute_public_delegated_prefixes.md
@@ -17,15 +17,19 @@ A `google_compute_public_delegated_prefixes` is used to test a Google PublicDele
 Properties that can be accessed from the `google_compute_public_delegated_prefixes` resource:
 
 See [google_compute_public_delegated_prefix.md](google_compute_public_delegated_prefix.md) for more detailed information
-  * `creation_timestamps`: an array of `google_compute_public_delegated_prefix` creation_timestamp
-  * `descriptions`: an array of `google_compute_public_delegated_prefix` description
+  * `kinds`: an array of `google_compute_public_delegated_prefix` kind
   * `ids`: an array of `google_compute_public_delegated_prefix` id
+  * `creation_timestamps`: an array of `google_compute_public_delegated_prefix` creation_timestamp
   * `names`: an array of `google_compute_public_delegated_prefix` name
+  * `descriptions`: an array of `google_compute_public_delegated_prefix` description
+  * `self_links`: an array of `google_compute_public_delegated_prefix` self_link
+  * `regions`: an array of `google_compute_public_delegated_prefix` region
   * `ip_cidr_ranges`: an array of `google_compute_public_delegated_prefix` ip_cidr_range
+  * `statuses`: an array of `google_compute_public_delegated_prefix` status
   * `parent_prefixes`: an array of `google_compute_public_delegated_prefix` parent_prefix
+  * `public_delegated_sub_prefixs`: an array of `google_compute_public_delegated_prefix` public_delegated_sub_prefixs
   * `is_live_migrations`: an array of `google_compute_public_delegated_prefix` is_live_migration
   * `fingerprints`: an array of `google_compute_public_delegated_prefix` fingerprint
-  * `statuses`: an array of `google_compute_public_delegated_prefix` status
 
 ## Filter Criteria
 This resource supports all of the above properties as filter criteria, which can be used
--- libraries/google_compute_public_delegated_prefixes.rb
+++ libraries/google_compute_public_delegated_prefixes.rb
@@ -23,15 +23,19 @@ class ComputePublicDelegatedPrefixs < GcpResourceBase
 
   filter_table_config = FilterTable.create
 
-  filter_table_config.add(:creation_timestamps, field: :creation_timestamp)
-  filter_table_config.add(:descriptions, field: :description)
+  filter_table_config.add(:kinds, field: :kind)
   filter_table_config.add(:ids, field: :id)
+  filter_table_config.add(:creation_timestamps, field: :creation_timestamp)
   filter_table_config.add(:names, field: :name)
+  filter_table_config.add(:descriptions, field: :description)
+  filter_table_config.add(:self_links, field: :self_link)
+  filter_table_config.add(:regions, field: :region)
   filter_table_config.add(:ip_cidr_ranges, field: :ip_cidr_range)
+  filter_table_config.add(:statuses, field: :status)
   filter_table_config.add(:parent_prefixes, field: :parent_prefix)
+  filter_table_config.add(:public_delegated_sub_prefixs, field: :public_delegated_sub_prefixs)
   filter_table_config.add(:is_live_migrations, field: :is_live_migration)
   filter_table_config.add(:fingerprints, field: :fingerprint)
-  filter_table_config.add(:statuses, field: :status)
 
   filter_table_config.connect(self, :table)
 
@@ -71,21 +75,20 @@ class ComputePublicDelegatedPrefixs < GcpResourceBase
 
   def transformers
     {
-      'creationTimestamp' => ->(obj) { return :creation_timestamp, parse_time_string(obj['creationTimestamp']) },
-      'description' => ->(obj) { return :description, obj['description'] },
+      'kind' => ->(obj) { return :kind, obj['kind'] },
       'id' => ->(obj) { return :id, obj['id'] },
+      'creationTimestamp' => ->(obj) { return :creation_timestamp, obj['creationTimestamp'] },
       'name' => ->(obj) { return :name, obj['name'] },
+      'description' => ->(obj) { return :description, obj['description'] },
+      'selfLink' => ->(obj) { return :self_link, obj['selfLink'] },
+      'region' => ->(obj) { return :region, obj['region'] },
       'ipCidrRange' => ->(obj) { return :ip_cidr_range, obj['ipCidrRange'] },
+      'status' => ->(obj) { return :status, obj['status'] },
       'parentPrefix' => ->(obj) { return :parent_prefix, obj['parentPrefix'] },
+      'publicDelegatedSubPrefixs' => ->(obj) { return :public_delegated_sub_prefixs, GoogleInSpec::Compute::Property::PublicDelegatedPrefixPublicDelegatedSubPrefixsArray.parse(obj['publicDelegatedSubPrefixs'], to_s) },
       'isLiveMigration' => ->(obj) { return :is_live_migration, obj['isLiveMigration'] },
       'fingerprint' => ->(obj) { return :fingerprint, obj['fingerprint'] },
-      'status' => ->(obj) { return :status, obj['status'] },
     }
-  end
-
-  # Handles parsing RFC3339 time string
-  def parse_time_string(time_string)
-    time_string ? Time.parse(time_string) : nil
   end
 
   private
@@ -95,6 +98,6 @@ class ComputePublicDelegatedPrefixs < GcpResourceBase
   end
 
   def resource_base_url
-    'projects/{{project}}/regions/{{region}}/publicDelegatedPrefixes'
+    'projects/{{project}}/regions/{{region}}/publicDelegatedPrefixes/{{publicDelegatedPrefix}}'
   end
 end
