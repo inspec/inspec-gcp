3c172da0d473cc50c37add0b913c175e5b1c2e78
Samir Anand <sam.sunny001@gmail.com>
2023-09-07 11:18:19 +0530

    Automatically generated by magic modules for service: vertex_ai and resource: Dataset. This commit includes the following changes: - Singular Resource - Plural Resource - Documentation updates - Terraform configuration - Integration tests
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- build/inspec/test/integration/build/gcp-mm.tf
+++ build/inspec/test/integration/build/gcp-mm.tf
@@ -1405,3 +1405,10 @@ resource "google_healthcare_dataset" "default" {
   time_zone = "UTC"
 }
 
+
+resource "google_healthcare_dataset" "default" {
+  name      = "example-dataset-${local.name_suffix}"
+  location  = "us-central1"
+  time_zone = "UTC"
+}
+
