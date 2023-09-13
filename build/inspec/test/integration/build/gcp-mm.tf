c664bc5ffc2b55e1d732b130a7ea7fbb50eedb2f
Samir Anand <sam.sunny001@gmail.com>
2023-09-13 23:43:55 +0530

    Automatically generated by magic modules for service: vertex_ai and resource: Endpoint. This commit includes the following changes: - Singular Resource - Plural Resource - Documentation updates - Terraform configuration - Integration tests
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- build/inspec/test/integration/build/gcp-mm.tf
+++ build/inspec/test/integration/build/gcp-mm.tf
@@ -1360,16 +1360,56 @@ resource "google_healthcare_dataset" "default" {
 }
 
 
-resource "google_healthcare_dataset" "default" {
-  name      = "example-dataset-${local.name_suffix}"
-  location  = "us-central1"
-  time_zone = "UTC"
+resource "google_service_directory_namespace" "example" {
+  provider     = google-beta
+  namespace_id = "example-namespace-${local.name_suffix}"
+  location     = "us-central1"
+}
+
+resource "google_service_directory_service" "example" {
+  provider   = google-beta
+  service_id = "example-service-${local.name_suffix}"
+  namespace  = google_service_directory_namespace.example.id
+}
+
+resource "google_service_directory_endpoint" "example" {
+  provider    = google-beta
+  endpoint_id = "example-endpoint-${local.name_suffix}"
+  service     = google_service_directory_service.example.id
+
+  metadata = {
+    stage  = "prod"
+    region = "us-central1"
+  }
+
+  address = "1.2.3.4"
+  port    = 5353
 }
 
 
-resource "google_healthcare_dataset" "default" {
-  name      = "example-dataset-${local.name_suffix}"
-  location  = "us-central1"
-  time_zone = "UTC"
+resource "google_service_directory_namespace" "example" {
+  provider     = google-beta
+  namespace_id = "example-namespace-${local.name_suffix}"
+  location     = "us-central1"
+}
+
+resource "google_service_directory_service" "example" {
+  provider   = google-beta
+  service_id = "example-service-${local.name_suffix}"
+  namespace  = google_service_directory_namespace.example.id
+}
+
+resource "google_service_directory_endpoint" "example" {
+  provider    = google-beta
+  endpoint_id = "example-endpoint-${local.name_suffix}"
+  service     = google_service_directory_service.example.id
+
+  metadata = {
+    stage  = "prod"
+    region = "us-central1"
+  }
+
+  address = "1.2.3.4"
+  port    = 5353
 }
 
