e76ef5474adea4358be9b86f3bc8646a53510a88
Samir Anand <sam.sunny001@gmail.com>
2023-09-14 12:03:54 +0530

    Automatically generated by magic modules for service: vertex_ai and resource: Featurestore. This commit includes the following changes: - Singular Resource - Plural Resource - Documentation updates - Terraform configuration - Integration tests
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- build/inspec/test/integration/build/gcp-mm.tf
+++ build/inspec/test/integration/build/gcp-mm.tf
@@ -1360,16 +1360,32 @@ resource "google_healthcare_dataset" "default" {
 }
 
 
-resource "google_healthcare_dataset" "default" {
-  name      = "example-dataset-${local.name_suffix}"
-  location  = "us-central1"
-  time_zone = "UTC"
+resource "google_vertex_ai_featurestore" "featurestore" {
+  name     = "terraform-${local.name_suffix}"
+  labels = {
+    foo = "bar"
+  }
+  region   = "us-central1"
+  online_serving_config {
+    fixed_node_count = 2
+  }
 }
 
+resource "google_vertex_ai_featurestore_entitytype" "entity" {
+  name     = "terraform-${local.name_suffix}"
+  labels = {
+    foo = "bar"
+  }
+  featurestore = google_vertex_ai_featurestore.featurestore.id
+}
 
-resource "google_healthcare_dataset" "default" {
-  name      = "example-dataset-${local.name_suffix}"
-  location  = "us-central1"
-  time_zone = "UTC"
+resource "google_vertex_ai_featurestore_entitytype_feature" "feature" {
+  name     = "terraform-${local.name_suffix}"
+  labels = {
+    foo = "bar"
+  }
+  entitytype = google_vertex_ai_featurestore_entitytype.entity.id
+
+  value_type = "INT64_ARRAY"
 }
 
