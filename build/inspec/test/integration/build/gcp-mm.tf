6e47079082f7fd8e6d94c751014bef770d299862
Samir Anand <sam.sunny001@gmail.com>
2023-09-14 12:36:43 +0530

    Automatically generated by magic modules for service: vertex_ai and resource: Schedule. This commit includes the following changes: - Singular Resource - Plural Resource - Documentation updates - Terraform configuration - Integration tests
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- build/inspec/test/integration/build/gcp-mm.tf
+++ build/inspec/test/integration/build/gcp-mm.tf
@@ -1359,3 +1359,22 @@ resource "google_healthcare_dataset" "default" {
   time_zone = "UTC"
 }
 
+
+resource "google_cloud_scheduler_job" "job" {
+  name             = "test-job-${local.name_suffix}"
+  description      = "test http job"
+  schedule         = "*/8 * * * *"
+  time_zone        = "America/New_York"
+  attempt_deadline = "320s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://example.com/"
+    body        = base64encode("{\"foo\":\"bar\"}")
+  }
+}
+
