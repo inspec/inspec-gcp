059e0c520b3b19c128fd05b9f323b117c3553837
balasubramanian-s <balasubramanian.s@progress.com>
2023-09-21 21:09:56 +0530

    Updated attribute values
    Signed-off-by: balasubramanian-s <balasubramanian.s@progress.com>

--- test/integration/verify/controls/google_vertex_ai_studies_trials.rb
+++ test/integration/verify/controls/google_vertex_ai_studies_trials.rb
@@ -17,16 +17,17 @@ title 'Test GCP google_vertex_ai_studies_trials resource.'
 gcp_project_id = input(:gcp_project_id, value: 'gcp_project_id', description: 'The GCP project identifier.')
 
   studies_trial = input('studies_trial', value: {
-  "name": "value_name",
-  "region": "value_region",
-  "parent": "value_parent",
-  "start_time": "value_starttime",
-  "end_time": "value_endtime",
-  "infeasible_reason": "value_infeasiblereason",
-  "client_id": "value_clientid",
-  "custom_job": "value_customjob",
-  "state": "value_state",
-  "id": "value_id"
+    "name": "1",
+    "region": "us-central1",
+    "parent": "projects/165434197229/locations/us-central1/studies/2975668570413/trials/",
+    "study": "890385007008",
+    "start_time": "value_starttime",
+    "end_time": "value_endtime",
+    "infeasible_reason": "value_infeasiblereason",
+    "client_id": "value_clientid",
+    "custom_job": "value_customjob",
+    "state": "value_state",
+    "id": "1"
 }, description: 'studies_trial description')
 control 'google_vertex_ai_studies_trials-1.0' do
   impact 1.0
