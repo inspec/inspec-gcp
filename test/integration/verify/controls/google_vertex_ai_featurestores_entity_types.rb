091af225dbc1f2a55622f13939696202ea94ed91
Samir Anand <sam.sunny001@gmail.com>
2023-09-21 11:51:36 +0530

    Validation Complted and merge fixed

--- test/integration/verify/controls/google_vertex_ai_featurestores_entity_types.rb
+++ test/integration/verify/controls/google_vertex_ai_featurestores_entity_types.rb
@@ -17,19 +17,22 @@ title 'Test GCP google_vertex_ai_featurestores_entity_types resource.'
 gcp_project_id = input(:gcp_project_id, value: 'gcp_project_id', description: 'The GCP project identifier.')
 
   featurestores_entity_type = input('featurestores_entity_type', value: {
-  "name": "value_name",
-  "region": "value_region",
-  "parent": "value_parent",
-  "description": "value_description",
-  "create_time": "value_createtime",
-  "etag": "value_etag",
-  "update_time": "value_updatetime"
+    "name": "entity_type_1",
+    "region": "us-central1",
+    "parent": "value_parent",
+    "description": "value_description",
+    "create_time": "2023-09-14T07:42:36.438713Z",
+    "etag": "AMEw9yOTGVIBT_QQ17nzZOq_p46_Xnjm2ZnaYi__v3FPk_Ofs9zb_qoKYnz3CALlXqzV",
+    "update_time": "2023-09-14T07:42:36.438714Z",
+    "featurestore": "feature_store_1"
 }, description: 'featurestores_entity_type description')
 control 'google_vertex_ai_featurestores_entity_types-1.0' do
   impact 1.0
   title 'google_vertex_ai_featurestores_entity_types resource test'
 
-      describe google_vertex_ai_featurestores_entity_types(parent: "projects/#{gcp_project_id}/locations/#{featurestores_entity_type['region']}/featurestores/#{featurestores_entity_type['featurestore']}", region: featurestores_entity_type['region']) do
+  # require "pry";binding.pry
+  describe google_vertex_ai_featurestores_entity_types(parent: "projects/#{gcp_project_id}/locations/#{featurestores_entity_type['region']}/featurestores/#{featurestores_entity_type['featurestore']}", region: featurestores_entity_type['region']) do
       it { should exist }
-    end
+  end
 end
+google_vertex_ai_featurestores_entity_types(parent: "projects/#{gcp_project_id}/locations/#{featurestores_entity_type['region']}/featurestores/gcp_feature_store_1", region: featurestores_entity_type['region'])
