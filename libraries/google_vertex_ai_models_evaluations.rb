b3a3f40b94e1a7502102d80e849656fb6a2f06e8
Samir Anand <sam.sunny001@gmail.com>
2023-09-25 14:38:50 +0530

    Validation Complted and merge fixed
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- libraries/google_vertex_ai_models_evaluations.rb
+++ libraries/google_vertex_ai_models_evaluations.rb
@@ -40,7 +40,7 @@ class VertexAIModelsEvaluations < GcpResourceBase
   def initialize(params = {})
     super(params.merge({ use_http_transport: true }))
     @params = params
-    @table = fetch_wrapped_resource('modelsEvaluations')
+    @table = fetch_wrapped_resource('modelEvaluations')
   end
 
   def fetch_wrapped_resource(wrap_path)
