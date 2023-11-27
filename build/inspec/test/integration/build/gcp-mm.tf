8311bed361b38daf14f2d83e26b9eb38f2b3d703
Samir Anand <sam.sunny001@gmail.com>
2023-11-27 20:23:03 +0530

    Automatically generated by magic modules for service: apigee and resource: Organizations__apis__revision. This commit includes the following changes: - Singular Resource - Plural Resource - Documentation updates - Terraform configuration - Integration tests
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- build/inspec/test/integration/build/gcp-mm.tf
+++ build/inspec/test/integration/build/gcp-mm.tf
@@ -1430,3 +1430,124 @@ data "google_compute_network" "vertex_network" {
 
 data "google_project" "project" {}
 
+
+resource "google_service_directory_namespace" "example" {
+  provider     = google-beta
+  namespace_id = "example-namespace-${local.name_suffix}"
+  location     = "us-central1"
+}
+
+resource "google_service_directory_service" "example" {
+  provider   = google-beta
+  service_id = "example-service-${local.name_suffix}"
+  namespace  = google_service_directory_namespace.example.id
+}
+
+resource "google_service_directory_endpoint" "example" {
+  provider    = google-beta
+  endpoint_id = "example-endpoint-${local.name_suffix}"
+  service     = google_service_directory_service.example.id
+
+  metadata = {
+    stage  = "prod"
+    region = "us-central1"
+  }
+
+  address = "1.2.3.4"
+  port    = 5353
+}
+
+
+resource "google_service_directory_namespace" "example" {
+  provider     = google-beta
+  namespace_id = "example-namespace-${local.name_suffix}"
+  location     = "us-central1"
+}
+
+resource "google_service_directory_service" "example" {
+  provider   = google-beta
+  service_id = "example-service-${local.name_suffix}"
+  namespace  = google_service_directory_namespace.example.id
+}
+
+resource "google_service_directory_endpoint" "example" {
+  provider    = google-beta
+  endpoint_id = "example-endpoint-${local.name_suffix}"
+  service     = google_service_directory_service.example.id
+
+  metadata = {
+    stage  = "prod"
+    region = "us-central1"
+  }
+
+  address = "1.2.3.4"
+  port    = 5353
+}
+
+
+resource "google_service_directory_namespace" "example" {
+  provider     = google-beta
+  namespace_id = "example-namespace-${local.name_suffix}"
+  location     = "us-central1"
+}
+
+resource "google_service_directory_service" "example" {
+  provider   = google-beta
+  service_id = "example-service-${local.name_suffix}"
+  namespace  = google_service_directory_namespace.example.id
+}
+
+resource "google_service_directory_endpoint" "example" {
+  provider    = google-beta
+  endpoint_id = "example-endpoint-${local.name_suffix}"
+  service     = google_service_directory_service.example.id
+
+  metadata = {
+    stage  = "prod"
+    region = "us-central1"
+  }
+
+  address = "1.2.3.4"
+  port    = 5353
+}
+
+
+resource "google_service_directory_namespace" "example" {
+  provider     = google-beta
+  namespace_id = "example-namespace-${local.name_suffix}"
+  location     = "us-central1"
+}
+
+resource "google_service_directory_service" "example" {
+  provider   = google-beta
+  service_id = "example-service-${local.name_suffix}"
+  namespace  = google_service_directory_namespace.example.id
+}
+
+resource "google_service_directory_endpoint" "example" {
+  provider    = google-beta
+  endpoint_id = "example-endpoint-${local.name_suffix}"
+  service     = google_service_directory_service.example.id
+
+  metadata = {
+    stage  = "prod"
+    region = "us-central1"
+  }
+
+  address = "1.2.3.4"
+  port    = 5353
+}
+
+
+resource "google_datastore_index" "default" {
+  kind = "foo"
+  properties {
+    name = "property_a-${local.name_suffix}"
+    direction = "ASCENDING"
+  }
+  properties {
+    name = "property_b-${local.name_suffix}"
+    direction = "ASCENDING"
+  }
+}
+
