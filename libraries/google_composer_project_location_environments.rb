7504a12fd580be06d54899d02bcfc9d128f7900d
Sonu Saha <sonu.saha@progress.com>
2023-12-05 14:33:20 +0530

    fix: update wrap path for google_composer_project_location_environments
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- libraries/google_composer_project_location_environments.rb
+++ libraries/google_composer_project_location_environments.rb
@@ -38,7 +38,7 @@ class ComposerProjectLocationEnvironments < GcpResourceBase
   def initialize(params = {})
     super(params.merge({ use_http_transport: true }))
     @params = params
-    @table = fetch_wrapped_resource('projectLocationEnvironments')
+    @table = fetch_wrapped_resource('environments')
   end
 
   def fetch_wrapped_resource(wrap_path)
