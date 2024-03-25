b78858c9dafd8891e2b45c74ab431e68ad057ebc
Sonu Saha <sonu.saha@progress.com>
2024-03-25 17:28:43 +0530

    test&fix: fix wrap path and update test
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- libraries/google_compute_interconnect_attachments.rb
+++ libraries/google_compute_interconnect_attachments.rb
@@ -71,7 +71,7 @@ class ComputeInterconnectAttachments < GcpResourceBase
   def initialize(params = {})
     super(params.merge({ use_http_transport: true }))
     @params = params
-    @table = fetch_wrapped_resource('interconnectAttachments')
+    @table = fetch_wrapped_resource('items')
   end
 
   def fetch_wrapped_resource(wrap_path)
