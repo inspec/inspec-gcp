577eb0add3018984d3f635881ee1880c1c2c2610
Sonu Saha <sonu.saha@progress.com>
2024-03-27 01:13:29 +0530

    fix: update wrap path to match json key in response
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- libraries/google_compute_target_instances.rb
+++ libraries/google_compute_target_instances.rb
@@ -41,7 +41,7 @@ class ComputeTargetInstances < GcpResourceBase
   def initialize(params = {})
     super(params.merge({ use_http_transport: true }))
     @params = params
-    @table = fetch_wrapped_resource('targetInstances')
+    @table = fetch_wrapped_resource('items')
   end
 
   def fetch_wrapped_resource(wrap_path)
