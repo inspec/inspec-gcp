7321596e0b2c3ec336645c579dc69fddeaf99a17
balasubramanian-s <balasubramanian.s@progress.com>
2024-07-18 15:56:47 +0530

    add gemspec files
    Signed-off-by: balasubramanian-s <balasubramanian.s@progress.com>

---
+++ lib/inspec-gcp-resources/resource_pack.rb
@@ -0,0 +1,15 @@
+# TODO: Determine if next like is required
+# require "inspec/resource"
+
+module InspecPlugins::GcpResources
+  # This class will provide the actual CLI implementation.
+  # Its superclass is provided by another call to Inspec.plugin,
+  # this time with two args.  The first arg specifies we are requesting
+  # version 2 of the Plugins API.  The second says we are making a Resource
+  # Pack plugin component, so please make available any DSL needed
+  # for that.
+    class ResourcePack < Inspec.plugin(2, :resource_pack)
+
+    end
+  end
+end
